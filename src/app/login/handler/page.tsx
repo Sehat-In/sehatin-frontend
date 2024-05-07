a143412af83124a6267dfb009b6578229c7e1329
TehGaa <syosuasaputra1010@gmail.com>
2024-05-07 17:32:34 +0700

    trying to fix issue

--- src/app/login/handler/page.tsx
+++ src/app/login/handler/page.tsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 export default function LoginHandler() {
     const handleLogin = async () => {
         await axios
-            .get(process.env.NEXT_PUBLIC_API_URL + "/api/v1/auth/dummy")
+            .post(process.env.NEXT_PUBLIC_API_URL + "/api/v1/auth/dummy")
             .then(async (response) => {
                 await axios
                     .get("/api")
