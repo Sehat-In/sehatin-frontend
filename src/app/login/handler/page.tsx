27be06191c914bc1451a31ee3a285e8f2e7d6d00
TehGaa <syosuasaputra1010@gmail.com>
2024-05-07 16:54:14 +0700

    fix: trying to fix cookie problem

--- src/app/login/handler/page.tsx
+++ src/app/login/handler/page.tsx
@@ -3,18 +3,23 @@
 import axios from "axios";
 import { useEffect } from "react";
 
-export default function LoginHandler(){
-    useEffect(()=>{
-        axios.get('/api')
-        .then((response)=>{
-            const user = JSON.parse(response.data.user);
-            localStorage.setItem('user', JSON.stringify(user));
-            window.location.href = '/';
-        })
-        .catch((error)=>{
-            console.log(error);
-            window.location.href = '/login';
-        })
-    }, [])
-    return (<></>)
+export default function LoginHandler() {
+    useEffect(() => {
+        axios
+            .get(process.env.NEXT_PUBLIC_API_URL + "api/v1/auth/dummy")
+            .then((response) => {
+                axios
+                    .get("/api")
+                    .then((response) => {
+                        const user = JSON.parse(response.data.user);
+                        localStorage.setItem("user", JSON.stringify(user));
+                        window.location.href = "/";
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        window.location.href = "/login";
+                    });
+            });
+    }, []);
+    return <></>;
 }
