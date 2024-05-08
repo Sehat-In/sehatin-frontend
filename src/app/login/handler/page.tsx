cd92ad7fd8a68ba1456372669a4f51d7d66dcb2b
TehGaa <syosuasaputra1010@gmail.com>
2024-05-08 14:46:27 +0700

    debug

--- src/app/login/handler/page.tsx
+++ src/app/login/handler/page.tsx
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function LoginHandler() {
