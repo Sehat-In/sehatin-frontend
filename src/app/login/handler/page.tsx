dab2e0e37f6f34c56e1ba6010b78b4d72a3b963f
TehGaa <syosuasaputra1010@gmail.com>
2024-05-08 14:37:15 +0700

    debug

--- src/app/login/handler/page.tsx
+++ src/app/login/handler/page.tsx
@@ -2,14 +2,23 @@
 
 import axios from "axios";
 import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function LoginHandler() {
     const searchParams = useSearchParams();
+    const router = useRouter();
     useEffect(() => {
         const search = searchParams.get('key') || 'ga ada';
-        console.log(search)
-        localStorage.setItem('key', search);
+        axios.get(process.env.NEXT_PUBLIC_API_URL + '/api/v1/auth/get-data-from-token', {
+            headers: {
+                Authorization: `Bearer ${search}`
+            }
+        })
+            .then((response)=>{
+                localStorage.setItem('user', JSON.stringify(response.data));
+                router.push('/');
+            })
     }, [searchParams]);
     return <>
         <h1>Logging in...</h1>
