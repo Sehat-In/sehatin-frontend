42ea943096e25209717b5bde23d410f8564011bc
TehGaa <syosuasaputra1010@gmail.com>
2024-05-08 13:05:36 +0700

    debug

--- src/app/login/handler/page.tsx
+++ src/app/login/handler/page.tsx
@@ -7,8 +7,8 @@ export default function LoginHandler() {
     const handleLogin = async () => {
         await axios
             .post(process.env.NEXT_PUBLIC_API_URL + "/api/v1/auth/dummy")
-            .then(async (response) => {
-                console.log(response);
+            .then((response) => {
+                console.log(response.headers['set-cookie']);
             }); 
     };
 
