7c02a1c60560ccc8057fd89ef3bfe5d7e9f42070
TehGaa <syosuasaputra1010@gmail.com>
2024-05-25 20:02:41 +0700

    feat: add notification button

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,14 +2,26 @@
 
 import { useRouter } from "next/navigation";
 import { useUserContext } from "../context/UserContext";
-import { useEffect } from "react";
-import { CircularProgress } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { Button, Icon } from "@chakra-ui/react";
+import { Spinner } from '@chakra-ui/react'
+import { BellIcon } from "@chakra-ui/icons";
+import { IconButton } from '@chakra-ui/react'
+import axios from "axios";
 
 
 const Navbar = () => {
-    const {loading, isAuthenticated, logout} = useUserContext();
-
+    const {userData, loading, isAuthenticated, logout} = useUserContext();
+    const [notification, setNotification] = useState(false);
     useEffect(()=>{
+        axios.get(process.env.NEXT_PUBLIC_API_URL + '/api/v1/posts/check-notification/' + userData.username)
+            .then((res)=>{
+                setNotification(true);
+            })
+            .catch((err)=>{
+                console.log(err);
+                setNotification(false);
+            })
     }, [loading])
 
     const router = useRouter();
@@ -28,10 +40,13 @@ const Navbar = () => {
                 <div className="flex-1">
                     <a className="btn btn-ghost text-xl">Sehat-In</a>
                 </div>
-                <div className="flex-none">
+                <div className="flex-none" style={{gap: 10}}>
+                    {notification ? 
+                    <IconButton aria-label="notification-button" boxSize={7} isRound={true} variant='solid' colorScheme='red' icon={<Icon as={BellIcon} boxSize={5}/>} onClick={handleLogin} /> :
+                    <IconButton aria-label="notification-button" boxSize={7} isRound={true} variant='solid' colorScheme='gray' icon={<Icon as={BellIcon} boxSize={5}/>} onClick={handleLogin} />}
                     <ul className="menu menu-horizontal px-1" style={{alignItems: 'center'}}>
                         <li>
-                            {loading ? <div><CircularProgress isIndeterminate color='green.300' /></div> : 
+                            {loading ? <div><Spinner color='green.300' /></div> : 
                             (isAuthenticated ? <button className="btn" onClick={handleLogout}>Logout</button>:<button className="btn" onClick={handleLogin}>Login</button>)}
                         </li>
                     </ul>
