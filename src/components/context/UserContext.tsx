1ccb9ca123125032d6bb89a78e9b7d454da1e739
TehGaa <syosuasaputra1010@gmail.com>
2024-06-03 17:19:19 +0700

    feat: add register

--- src/components/context/UserContext.tsx
+++ src/components/context/UserContext.tsx
@@ -3,6 +3,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useContext } from "react";
+import { useToast } from '@chakra-ui/react'
 
 export interface UserData{
     id: number;
@@ -26,6 +27,7 @@ export interface User{
     isAuthenticated: boolean;
     login: (query:any)=>Promise<void>;
     logout: ()=>void;
+    register: (query:any)=>void;
 }
 
 export const UserContext = React.createContext<User>({} as User);
@@ -36,6 +38,7 @@ export const UserContextProvider: React.FC<any> = ({ children }:any) => {
     const [userData, setUserData] = useState<UserData>({} as UserData);
     const [loading, setLoading] = useState<boolean>(true);
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+    const toast = useToast();
 
 
     const login = async (query:any) => {
@@ -46,7 +49,35 @@ export const UserContextProvider: React.FC<any> = ({ children }:any) => {
         .then((response)=>{
             setUserData(response.data);
             localStorage.setItem('user', JSON.stringify(response.data));
-            window.location.href = '/';
+            toast({
+                title: 'Login Success!',
+                description: 'You have successfully logged in. Redirecting you to the homepage.',
+                status: 'success',
+                position: 'top-right',
+                isClosable: true,
+            });
+            setTimeout(() => {
+                window.location.href = '/';
+            }, 800);
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
+
+    const register = async (query:any) => {
+        await axios.post(process.env.NEXT_PUBLIC_API_URL + '/api/v1/auth/register', {
+            username: query.username,
+            password: query.password
+        })
+        .then((response)=>{
+            toast({
+                title: 'Register Success!',
+                description: 'You have successfully registered. Please login to continue.',
+                status: 'success',
+                position: 'top-right',
+                isClosable: true,
+            });
         })
         .catch((error)=>{
             console.log(error);
@@ -92,7 +123,7 @@ export const UserContextProvider: React.FC<any> = ({ children }:any) => {
 
     return (
     <>
-        <UserContext.Provider value={{userData: userData, loading, isAuthenticated, login, logout}}>
+        <UserContext.Provider value={{userData: userData, loading, isAuthenticated, login, logout, register}}>
             {children}
         </UserContext.Provider>
     </>)
